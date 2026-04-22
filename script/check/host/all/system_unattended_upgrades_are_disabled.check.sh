e2e1845ce9b994a7afb535e6bd725ccb648b41c4
m8in <m8in@gmx.de>
2026-04-22 21:21:39 +0200

    fixed check unattended upgrades disabled

--- script/check/host/all/system_unattended_upgrades_are_disabled.check.sh
+++ script/check/host/all/system_unattended_upgrades_are_disabled.check.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
-! systemctl is-enabled unattended-upgrades.service > /dev/null 2>&1 \
+#
+# There are three components working together:
+#   - systemctl status unattended-upgrades.service      (One-Shot-Service, loaded means system knows the service, it DOES NOT run in background)
+#   - systemctl status apt-daily.timer                  (apt update)
+#   - systemctl status apt-daily-upgrade.timer          (apt upgrade)
+#
+# So disable/enaable the Upgrade with:
+#   - systemctl disable --now apt-daily-upgrade.timer   (--now means disable and stop)
+#   - systemctl enable --now apt-daily-upgrade.timer    (--now means enable and start)
+#
+! systemctl is-enabled apt-daily-upgrade.timer > /dev/null 2>&1 \
     && exit 0
 exit 1
