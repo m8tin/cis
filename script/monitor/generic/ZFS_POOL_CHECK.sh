b6fe890e741a29fa16e23ddd0d21f812539db177
m8in <m8in@gmx.de>
2026-06-15 20:33:52 +0200

    Fixed generic monitor check ZFS_POOL_CHECK

--- script/monitor/generic/ZFS_POOL_CHECK.sh
+++ script/monitor/generic/ZFS_POOL_CHECK.sh
@@ -5,7 +5,7 @@ base.loadModule ssh
 
 
 function testPool(){
-    local _RESPONSE=$(ssh.onHostRun "monitoring@${1:?"Missing REMOTE_HOST"}" 'zpool status ${_ZFS_POOL} | grep -F scrub')
+    local _RESPONSE=$(ssh.onHostRun "monitoring@${1:?"Missing REMOTE_HOST"}" "zpool status ${2:?"Missing REMOTE_POOL"} | grep -F scrub")
     local _RESULT=$(echo "${_RESPONSE}" | grep -F 'scrub repaired 0B' | grep -F '0 errors')
     _RESULT="${_RESULT#*on}"  #Removes shortest matching pattern '*on' from the begin
 
@@ -18,6 +18,7 @@ function testPool(){
 }
 
 base.set REMOTE_HOST "${1:?"FQDN of server missing: e.g. host.example.net[:port]"}" '^([a-zA-Z0-9][a-zA-Z0-9.-]*)+(:[0-9]+)?$'
-testPool "${REMOTE_HOST}" && exit 0
+base.set REMOTE_POOL "${2:?"Missing name of zpool: e.g. zpool1"}" '^[a-zA-Z0-9_-]+$'
+testPool "${REMOTE_HOST}" "${REMOTE_POOL}" && exit 0
 
 exit 1
