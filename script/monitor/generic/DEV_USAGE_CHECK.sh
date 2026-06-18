7c8f9e1cfa1001f130be868103884286f7c3adb5
m8in <m8in@gmx.de>
2026-06-18 20:45:56 +0200

    ssh remote command of DEV_USAGE_CHECK.sh is much simpler now

--- script/monitor/generic/DEV_USAGE_CHECK.sh
+++ script/monitor/generic/DEV_USAGE_CHECK.sh
@@ -5,7 +5,8 @@ base.loadModule ssh
 
 
 function testSpace(){
-    local _RESULT=$(ssh.onHostRun "monitoring@${1:?"Missing REMOTE_HOST"}" 'df "/" | tail -n1 | tr -s "[:blank:]" " " | cut -d" " -f1,5')
+    local _RESPONSE=$(ssh.onHostRun "monitoring@${1:?"Missing REMOTE_HOST"}" 'df "/" | tail -n1')
+    local _RESULT=$(echo "${_RESPONSE}" | tr -s "[:blank:]" " " | cut -d" " -f1,5)
     local _DEV=$(echo "${_RESULT}" | tail -n 1 | cut -d' ' -f1)
     local _SPACE_USED=$(echo "${_RESULT}" | tail -n 1 | cut -d' ' -f2)
 
